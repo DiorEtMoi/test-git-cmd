6d0ad0510b2fd3858e9ab05622946682917ea84e
DiorEtMoi <dinhhoan0511@gmail.com>
2024-10-02 10:17:20 +0700

    test

--- src/Service/SelectionSort.java
+++ src/Service/SelectionSort.java
@@ -1,16 +1,16 @@
-//package Service;
-//
-//public class SelectionSort implements ISorter{
-//    @Override
-//    public void sort(int[] a) {
-//        for(int i = 0; i < a.length - 1; i++){
-//            for(int j = i; j < a.length; j++){
-//                if(a[i] > a[j]){
-//                    int temp = a[i];
-//                    a[i] = a[j];
-//                    a[j] = temp;
-//                }
-//            }
-//        }
-//    }
-//}
+package Service;
+
+public class SelectionSort implements ISorter{
+    @Override
+    public void sort(int[] a) {
+        for(int i = 0; i < a.length - 1; i++){
+            for(int j = i; j < a.length; j++){
+                if(a[i] > a[j]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+}
